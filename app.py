2aa50dfde8f1a9d08d5e827ab025f73b8388c3f1
Muhamad Rosif Al Khikam <110785229+MuhamadRosif@users.noreply.github.com>
2025-10-26 16:06:30 +0700

    31

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from kasir_mas_ragil import admin_app, user_app
 st.set_page_config(page_title="Kasir Mas Ragil", page_icon="🍜", layout="wide")
 
 # Pilih role
-role_choice = st.radio("Login sebagai:", ["Admin", "User"], key="role_select")
+role_choice = st.radio("Login sebagai:", ["Admin", "User"], key="role_select_main")
 
 if role_choice == "Admin":
     admin_app.run_admin()
 else:
-    user_app.run_user() 
+    user_app.run_user()
