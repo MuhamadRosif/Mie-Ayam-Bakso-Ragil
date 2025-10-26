63aead204ab2e7b2ac441fe9536363b8c9df370a
Muhamad Rosif Al Khikam <110785229+MuhamadRosif@users.noreply.github.com>
2025-10-26 15:45:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ from kasir_mas_ragil import admin_app, user_app
 
 st.set_page_config(page_title="Kasir Mas Ragil", page_icon="🍜", layout="wide")
 
-# Session state default
-if "admin_login" not in st.session_state:
-    st.session_state.admin_login = False
-if "user_login" not in st.session_state:
-    st.session_state.user_login = False
-
 # Pilih role
-role_choice = st.radio("Login sebagai:", ["Admin", "User"], key="role_select_unique")
+role_choice = st.radio("Login sebagai:", ["Admin", "User"], key="role_select")
 
 if role_choice == "Admin":
     admin_app.run_admin()
 else:
-    user_app.run_user()
+    user_app.run_user() 
