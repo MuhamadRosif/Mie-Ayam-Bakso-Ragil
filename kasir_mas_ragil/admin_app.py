4db0cf17dce9bbe93467413ca055350da9e021a9
Muhamad Rosif Al Khikam <110785229+MuhamadRosif@users.noreply.github.com>
2025-10-26 14:22:07 +0700

    Create admin_app.py

---
+++ kasir_mas_ragil/admin_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run_admin_app():
+    st.title("🛠️ Admin Dashboard")
+    st.write("Di sini admin bisa lihat laporan, update menu, hapus, dll.")
+    # Bisa masukkan semua kode admin yang sebelumnya di app.py
