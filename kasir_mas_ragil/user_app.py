0b0cbaa1ee6283a8f3655e0fd8854e26a3676881
Muhamad Rosif Al Khikam <110785229+MuhamadRosif@users.noreply.github.com>
2025-10-26 14:23:03 +0700

    Create user_app.py

---
+++ kasir_mas_ragil/user_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run_user_app():
+    st.title("🍜 User Kasir Mas Ragil")
+    st.write("Di sini user bisa pesan makanan, lihat keranjang, dan checkout.")
+    # Masukkan kode user: beranda, pesan menu, keranjang
